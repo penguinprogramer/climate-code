ebb7db82337689c8179dda973ea3adda5d70799b
Alexo <alexander.c.owusu@gmail.com>
2021-08-07 08:31:31 +0100

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,22 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/company/:name",
+    name: "Company",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Company.vue"),
+  },
+  {
+    path: "/product/name",
+    name: "Product",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Product.vue"),
   },
 ];
 
